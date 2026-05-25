9662257c0e87fa631c1bb108299cdd15ec562f5a
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2026-05-25 14:15:16 +0700

    try: make script build crash and roleback version

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import "testing-maybe-become-a-crash"
 
 // https://vite.dev/config/
 export default defineConfig({
